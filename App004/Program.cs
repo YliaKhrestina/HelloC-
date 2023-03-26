8957ec1d864878d952b1da8c5ce1e5518bd61098
Ylia <ukhrestina@yandex.ru>
2023-03-26 18:38:00 +0300

    дополнительная задача, внесено изменение в код

--- App004/Program.cs
+++ App004/Program.cs
@@ -1,8 +1,21 @@
 ﻿// два числа, ищем меньшее, к нему прибавляем разницу между большим и меньшим, столько раз, чему равняется меньшее
 
-int a = 0, b = 0, count = 0, result = 0;
+int a = 0, b = 0, result = 0;
 
-if (int.TryParse(Console.ReadLine(), out a) && if int.TryParse(Console.ReadLine(), out ) && a != b)
+if (int.TryParse(Console.ReadLine(), out a) && int.TryParse(Console.ReadLine(), out b) && a != b)
 {
-    
+    if (a < b)
+    {
+        result = a + b * (b-a);
+        Console.WriteLine(result);
+    }
+    else
+    {
+        result = b + b * (a - b);
+        Console.WriteLine(result);
+    }
+}
+else
+{
+    Console.WriteLine("OOps");
 }
