b2b87cb05742b938ee3213a9839dcec5bd058412
Ylia <ukhrestina@yandex.ru>
2023-03-26 17:11:33 +0300

    square number

---
+++ App001/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа, которвя на вход принимает число и выдает его квадрат.
+
+int i = 0;
+
+if (int.TryParse(Console.ReadLine(), out i))
+{
+    Console.WriteLine(i * i);
+}
+else
+{
+    Console.WriteLine("OOps");
+}
