f536ba2ef1e1e910ee3fd45a390c36e9fcbd255d
Ylia <ukhrestina@yandex.ru>
2023-03-26 18:25:53 +0300

    выводим название дня недели

---
+++ App002/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> да
+//a = 2, b = 10 -> нет
+//a = 9, b = -3 -> да
+//a = -3 b = 9 -> нет
+
+int a = 0, b = 0;
+
+if (int.TryParse(Console.ReadLine(), out a) && int.TryParse(Console.ReadLine(), out b))
+{
+    if (a*a==b)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+else
+{
+    Console.WriteLine("OOps");
+}
---
+++ App003/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//3 -> Среда
+//5 -> Пятница
+
+int a = 0;
+
+if (int.TryParse(Console.ReadLine(), out a) && a >= 1 && a <= 7)
+{
+    if (a == 1)
+    {
+        Console.Write("понедельник");
+    }
+    else if (a == 2)
+    {
+        Console.Write("вторник");
+    }
+    else if (a == 3)
+    {
+        Console.Write("среда");
+    }
+    else if (a == 4)
+    {
+        Console.Write("четверг");
+    }
+    else if (a == 5)
+    {
+        Console.Write("пятница");
+    }
+    else if (a == 6)
+    {
+        Console.Write("суббота");
+    }
+    else if (a == 7)
+    {
+        Console.Write("воскресенье");
+    }
+}
+else
+{
+    Console.Write("OOps");
+}
