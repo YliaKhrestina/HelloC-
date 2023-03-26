df37c3386254c9b161332ac4e731930ac381ee03
Ylia <ukhrestina@yandex.ru>
2023-03-26 18:27:50 +0300

    дополнительная задача на семинаре

---
+++ App004/Program.cs
@@ -0,0 +1,8 @@
+﻿// два числа, ищем меньшее, к нему прибавляем разницу между большим и меньшим, столько раз, чему равняется меньшее
+
+int a = 0, b = 0, count = 0, result = 0;
+
+if (int.TryParse(Console.ReadLine(), out a) && if int.TryParse(Console.ReadLine(), out ) && a != b)
+{
+    
+}
