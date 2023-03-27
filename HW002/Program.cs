d395ab2c7aecaf740a89b3b1e1bd5110e4d6f2b9
Ylia <ukhrestina@yandex.ru>
2023-03-27 19:45:22 +0300

    задача ДЗ вывод большего из 3х чисел

---
+++ HW002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает 3 числа и выводит максимальное
+
+System.Console.WriteLine("Введите число  ");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+
+System.Console.Write("max = ");
+System.Console.Write(max);
