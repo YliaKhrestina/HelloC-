dbde49201e4e8dc0ff5c3b3587dad98b5726196b
Ylia <ukhrestina@yandex.ru>
2023-03-27 20:07:25 +0300

    задача ДЗ определить четность и нечетность числа

---
+++ HW003/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится на 2 без остатка)
+
+System.Console.WriteLine("Введите число  ");
+
+int a = int.Parse(Console.ReadLine());
+if (a%2 == 0)
+{
+    System.Console.WriteLine("четное");
+}
+else
+{
+    System.Console.WriteLine("нечетное");
+}
