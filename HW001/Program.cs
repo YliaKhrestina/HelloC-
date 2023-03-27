2f3dd9f97ed8bc39e6c0dd75f64f9745f9caf082
Ylia <ukhrestina@yandex.ru>
2023-03-27 19:34:05 +0300

    задача ДЗ вывод большего и меньшего

---
+++ HW001/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу, которая на вход принимает 2 числа и выдает какое больше, а какое меньше
+
+int a = 1;
+int b = 15;
+if (a!=b)
+{
+    if (a > b)
+    {
+        System.Console.WriteLine("max = a");
+        System.Console.WriteLine("min = b");
+    }
+    else
+    {
+        System.Console.WriteLine("max = b");
+        System.Console.WriteLine("min = a");
+    }
+}
