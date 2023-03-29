f1a8d0f713dc0d7189b0199f63a2bcd79f2c90d0
Ylia <ukhrestina@yandex.ru>
2023-03-29 17:13:39 +0300

    задача ДЗ вывести все четные числа массива

---
+++ HW004/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число N, а на выход все четные числа от 1 до N
+
+System.Console.WriteLine("Введите число  ");
+
+int a = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= a; i+=2)
+{
+    System.Console.Write(i+",");
+}
