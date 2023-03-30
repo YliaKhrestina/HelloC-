e8add62fefa1776eb5a3cf7f8f25d3c3fdb69fd9
Ylia <ukhrestina@yandex.ru>
2023-03-30 23:01:02 +0300

    выдаёт таблицу квадратов чисел от 1 до N 2й вариант

---
+++ App011/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+
+int N = GetInt();
+
+int GetInt()
+{
+    System.Console.WriteLine("Введите целое число: ");
+    int N = int.Parse(Console.ReadLine());
+    return N;
+}
+
+string SquareNumber(int N)
+{
+    string result = "";
+    for (int n = 1; n <= N; n++)
+    {
+        result += Math.Pow(n, 2)+" ";
+    }
+    return result;
+}
+
+System.Console.WriteLine(SquareNumber(N));
