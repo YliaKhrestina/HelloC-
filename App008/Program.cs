1c91d8aa5e0e85843b5ee6a32b069feeaf57a9aa
Ylia <ukhrestina@yandex.ru>
2023-03-30 22:58:08 +0300

    показывает диапазон возможных координат точек в этой четверти (x и y)

---
+++ App008/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+int Read()
+
+{
+    System.Console.WriteLine($"Введите номер четверти:");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+System.Console.WriteLine(QouteNumb(Read()));
+
+String QouteNumb( int num)
+{
+    if (num == 1) return ("x>0; y>0"); 
+    else if (num == 2) return ("x<0; y>0");   
+    else if (num == 3) return ("x<0; y<0");
+    else return ("x>0; y<0"); 
+}
