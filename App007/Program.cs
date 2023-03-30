9093579cf74e8d13e633758327e6f2611aa3cba1
Ylia <ukhrestina@yandex.ru>
2023-03-30 22:56:50 +0300

    выдаёт таблицу квадратов чисел от 1 до N

---
+++ App007/Program.cs
@@ -0,0 +1,22 @@
+﻿int Read(string coordinate)
+{
+    System.Console.WriteLine($"Введите {coordinate}:");
+    int x = int.Parse(Console.ReadLine());
+    return x;
+}
+
+int x = Read("x");
+int y = Read("y");
+
+int QouteNumb( int x, int y)
+{
+    if (x>0 && y>0) return 1;
+    else if (x>0 && y<0) return 4;
+    else if (x<0 && y>0) return 2;
+    else return 3;
+}
+
+if (x==0 || y == 0) System.Console.WriteLine("Точка лежит на осях координат и не принадлежит четверти");
+else System.Console.WriteLine($"Точка с координатами {x} {y} лежит в четверти - {QouteNumb(x,y)}");
+Kirill_Borisenko 20:22
+Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
